5f7a6c46757f31483de5d8e67152bef8b927e761
exultant123 <salikh.ibragimov@std.ieu.edu.tr>
2026-02-27 16:49:11 +0300

    Delete lab directory

--- lab/myFirstLabScript.py
+++
@@ -1,4 +0,0 @@
-name = input("What is your name?\n")
-print("Hello " +name +".")
-id1 = input("What is your Student ID?\n")
-print("Your ID is " + id1 + ".")
--- lab/time_logic.py
+++
@@ -1,7 +0,0 @@
-time = input("Enter time in seconds\n")
-time = int(time)
-hours = int(time)/3600
-minutes = (time - int(hours)*3600)/60
-seconds = (time - int(hours)*3600 -int(minutes)*60)
-print(str(time)+" seconds is "+str(int(hours)) +" hours, " + str(int(minutes)) +" minutes and " + str(int(seconds)) +" seconds\n")
-
