f9cf3b9b184e45b9983dd784a08de82c6deeeadd
exultant123 <salikh.ibragimov@std.ieu.edu.tr>
2026-02-27 16:50:09 +0300

    Add files via upload

---
+++ lab1/coordinate_geometry.py
@@ -0,0 +1,7 @@
+x1 = float(input("Enter x1\n"))
+y1 = float(input("Enter y1\n"))
+x2 = float(input("Enter x2\n"))
+y2 = float(input("Enter y2\n"))
+dist = (x2-x1)**2+(y2-y1)**2
+dist = dist**0.5
+print("Distance is " + str(dist) +"\n")
